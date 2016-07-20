66b1715e1316d64e8d01a916d234a25b14bc70ad
jgyim <jgyim@naver.com>
2016-07-20 13:48:42 +0900

    second test

--- src/main/java/com/multicampus/biz/board/BoardDAO.java
+++ src/main/java/com/multicampus/biz/board/BoardDAO.java
@@ -16,7 +16,7 @@ public class BoardDAO {
 	// JDBC 관련 변수
 	private Connection conn = null;
 	private PreparedStatement stmt = null;
-	private ResultSet rs = null;
+	private ResultSet rs = null; 
 
 	// SQL 명령어들
 	private final String BOARD_INSERT = "insert into board(seq, title, writer, content) values((select nvl(max(seq), 0)+1 from board),?,?,?)";
